915749c37a8d6c228f3178263edfc8deecde51cf
JuliaGolota <golotajulia@gmail.com>
2025-03-30 22:37:47 +0200

    completed task-1

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,4 +2,10 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum
+let sum = 0;
+let i = 1;
+while (i <= 20) {
+    i ++;        // i = i + 1 (i += 1)
+    sum += i;   // sum = sum + i
+}
+console.log(sum);
