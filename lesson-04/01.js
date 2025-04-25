f0dee90c484b8325f137f98a36e10ebdc9f16b91
JuliaGolota <golotajulia@gmail.com>
2025-04-26 00:41:19 +0300

    correction lesson-04/01.js

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -16,12 +16,13 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 
 function includesElement(array, element) {
     for (let i = 0; i < array.length; i++) {
-        if (element) {          
-            return true;
-        }else{
-            return false;
-        }       
+        if (array[i] === element) { 
+            return true;      
+        }             
     }
+    return false;  
 }
 console.log(includesElement([1, 2, 3], 2));
+
+
 
