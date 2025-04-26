cfa47d6d1bb5efa276c14f9397c32fe86cff10d9
JuliaGolota <golotajulia@gmail.com>
2025-04-26 22:53:49 +0300

    completed lesson-04/02.js

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `findUniqueElements`, которая принимает массив и возвращает новый массив, содержащий только уникальные элементы из исходного массива.
+Напишите функцию `findUniqueElements`, которая принимает массив и возвращает новый массив, содержащий 
+только уникальные элементы из исходного массива.
 
 **Входные данные:**
 
@@ -12,7 +13,30 @@
 **Пример использования:**
 `findUniqueElements([1, 2, 3, 2, 1, 4])` вернёт `[1, 2, 3, 4]`.
 
-Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
+Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, 
+она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+// 1. Функция поиска элемента
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) { 
+            return true;      
+        }             
+    }
+    return false;  
+}
+
+// 2. Функция поиска уникальных элементов
+function findUniqueElements(array) {
+    let uniqueArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let isIncluded = includesElement(uniqueArray, array[i])
+        if(!isIncluded){
+            uniqueArray.push(array[i]);
+        }
+    }
+    return uniqueArray;
+}
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4]));
+
